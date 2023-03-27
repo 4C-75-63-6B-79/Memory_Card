259773c232dcf5606ce5a0440cca3a63943886cc
4C-75-63-6B-79 <luckyhairybeast987654@gmail.com>
2023-03-27 10:10:27 +0530

    created the function that when I press button that new Images are being rendered on scree

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import "./App.css";
+import { useState } from "react";
+import { getCharacterAtIndex, getRandomArray } from "./modules/helper_function";
+
+import Image from "./components/image";
+
+
 
 function App() {
+  
+  const [charcDataArr, setCharcDataArr] = useState([]);
+
+  function onButtonClick() {
+    const randomNumArray = getRandomArray(4);
+    const characterDataArrayPromises = randomNumArray.map((index) => getCharacterAtIndex(index));
+    Promise.all(characterDataArrayPromises).then((dataArray) => setCharcDataArr(dataArray));
+  }
+
   return (
     <div className="App">
       {"hello"}
+      <button onClick={onButtonClick}>Click</button>
+      {
+        charcDataArr.map((characterData) => <Image key={characterData.id} src={characterData.image} />)
+      }
     </div>
   );
 }
